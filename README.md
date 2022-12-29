3851b6781796b75e00e8a716c2c9debafc9f7c7a
Văn Thanh <vanthanh@jv-it.com.vn>
2022-12-29 13:29:14 +0700

    set up view engine with express

---
+++ README.md
@@ -0,0 +1,10 @@
+<!--
+* Các npm cần run
+1/ npm install -save-exact express@4.17.1
+=> express fw
+2/ npm install --save-exact ejs@3.1.6
+=> template express EJS "link github: https://github.com/mde/ejs"
+3/ npm install --save-exact body-parser@1.19.0 nodemon@2.0.12 @babel/core@7.15.5 @babel/node@7.15.4 @babel/preset-env@7.15.6
+=> 5 package use fix lỗi import do verison cũ
+
+ -->
--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
-const express = require('express')
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+
 const app = express()
 const port = 8080
 
+configViewEngine(app);
+
 app.get('/', (req, res) => {
-  res.send('Yowai mo!')
+    res.render('index.ejs')
 })
 
 app.get('/about', (req, res) => {
     res.send(`I'm thanhrain`)
-  })
+})
 
 app.listen(port, () => {
   console.log(`Thanhrain port ${port}`)
